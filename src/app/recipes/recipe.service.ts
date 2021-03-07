ccf97fa532af356a589089ca5e65532f5222577b
Akshay Patil <akshay.patil3096@gmail.com>
2021-03-07 23:34:34 +0530

    tt

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -12,7 +12,7 @@ export class RecipeService {
             'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.826.620.suffix/1537973085542.jpeg',
             [
                 new Ingredients('Tomatos', 5),
-                new Ingredients('Potato', 3)
+                new Ingredients('Potatos', 3)
             ]
         ),
         new Recipe('A Test Recipe 2',
